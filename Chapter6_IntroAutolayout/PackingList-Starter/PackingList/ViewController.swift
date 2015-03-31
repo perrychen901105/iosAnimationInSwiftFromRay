98795518be9620c23e5f1bb43fce7d6537a42973
Perry <chenzhipeng901105@gmail.com>
2015-03-31 17:41:15 +0800

    add layout constraint

--- Chapter6_IntroAutolayout/PackingList-Starter/PackingList/ViewController.swift
+++ Chapter6_IntroAutolayout/PackingList-Starter/PackingList/ViewController.swift
@@ -30,6 +30,8 @@ class ViewController: UIViewController {
   @IBOutlet var buttonMenu: UIButton!
   @IBOutlet var titleLabel: UILabel!
   
+    @IBOutlet var menuHeightConstraint: NSLayoutConstraint!
+    
   //MARK: further class variables
   
   var slider: HorizontalItemList!
@@ -39,12 +41,89 @@ class ViewController: UIViewController {
   //MARK: class methods
   
   @IBAction func actionToggleMenu(sender: AnyObject) {
+    for c in titleLabel.superview!.constraints() {
+        println(c)
+    }
+    isMenuOpen = !isMenuOpen
+    for constraint in titleLabel.superview!.constraints() as [NSLayoutConstraint] {
+        if constraint.secondItem as? NSObject == titleLabel && constraint.secondAttribute == .CenterX {
+            constraint.constant = isMenuOpen ? 100.0 : 0.0
+            continue
+        }
+        if constraint.firstItem as? NSObject == titleLabel && constraint.firstAttribute == .CenterY {
+            titleLabel.superview!.removeConstraint(constraint)
+            
+            let newConstraint = NSLayoutConstraint(item: titleLabel, attribute: .CenterY, relatedBy: .Equal, toItem: titleLabel.superview!, attribute: .CenterY, multiplier: isMenuOpen ? 0.67 : 1.0, constant: 5.0)
+            // tell Auto Layout to apply it to the current layout
+            newConstraint.active = true
+            
+            continue
+        }
+    }
+    menuHeightConstraint.constant = isMenuOpen ? 200.0 : 60.0
+    titleLabel.text = isMenuOpen ? "Select Item" : "Packing Lists"
+    
+    UIView.animateWithDuration(1.0, delay: 0.0, usingSpringWithDamping: 0.4, initialSpringVelocity: 10.0, options: .CurveEaseIn, animations: { () -> Void in
+        let angle = self.isMenuOpen ? CGFloat(M_PI_4) : 0.0
+        self.buttonMenu.transform = CGAffineTransformMakeRotation(angle)
+        self.view.layoutIfNeeded()
+        
+        }, completion: { _ in
+            
+        })
+    
+    if isMenuOpen {
+        slider = HorizontalItemList(inView: view)
+        slider.didSelectItem = {index in
+            println("add \(index)")
+            self.items.append(index)
+            self.tableView.reloadData()
+            self.actionToggleMenu(self);
+        }
+        self.titleLabel.superview!.addSubview(slider)
+    } else {
+        slider.removeFromSuperview()
+    }
     
   }
   
   func showItem(index: Int) {
     println("tapped item \(index)")
+    let imageView = UIImageView(image: UIImage(named: "summericons_100px_0\(index).png"))
+    imageView.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5)
+    imageView.layer.cornerRadius = 5.0
+    imageView.layer.masksToBounds = true
+    imageView.setTranslatesAutoresizingMaskIntoConstraints(false)
+    view.addSubview(imageView)
     
+    // center image x
+    let conX = NSLayoutConstraint(item: imageView, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1.0, constant: 0.0)
+    conX.active = true
+    
+    let conY = NSLayoutConstraint(item: imageView, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1.0, constant: imageView.frame.size.height)
+    conY.active = true
+    
+    let conWidth = NSLayoutConstraint(item: imageView, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 0.33, constant: -50.0)
+    conWidth.active = true
+    
+    let conHeight = NSLayoutConstraint(item: imageView, attribute: .Height, relatedBy: .Equal, toItem: imageView, attribute: .Width, multiplier: 1.0, constant: 0.0)
+    conHeight.active = true
+    
+    view.layoutIfNeeded()
+
+    UIView.animateWithDuration(0.8, delay: 0.0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.0, options: nil, animations: { () -> Void in
+        conY.constant = -imageView.frame.size.height / 2
+        conWidth.constant = 0.0
+        self.view.layoutIfNeeded()
+    }, completion: nil)
+    
+    UIView.animateWithDuration(0.8, delay: 1.0, options: nil, animations: { () -> Void in
+        conY.constant = imageView.frame.size.height
+        conWidth.constant = -50.0;
+        self.view.layoutIfNeeded()
+        }, completion: { _ in
+            imageView.removeFromSuperview()
+        })
   }
 }
 
