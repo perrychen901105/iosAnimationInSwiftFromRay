bf684e807baa6536822239f12847a5a0170f5ff5
Perry <chenzhipeng901105@gmail.com>
2015-04-13 18:27:25 +0800

    add chapter 17 code

---
+++ Chapter17_ParticleEmitters/Flight Info/ViewController.swift
@@ -0,0 +1,103 @@
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import UIKit
+import QuartzCore
+
+//
+// Util delay function
+//
+func delay(#seconds: Double, completion:()->()) {
+  let popTime = dispatch_time(DISPATCH_TIME_NOW, Int64( Double(NSEC_PER_SEC) * seconds ))
+  
+  dispatch_after(popTime, dispatch_get_main_queue()) {
+    completion()
+  }
+}
+
+class ViewController: UIViewController {
+  
+  @IBOutlet var bgImageView: UIImageView!
+  
+  @IBOutlet var summaryIcon: UIImageView!
+  @IBOutlet var summary: UILabel!
+  
+  @IBOutlet var flightNr: UILabel!
+  @IBOutlet var gateNr: UILabel!
+  @IBOutlet var departingFrom: UILabel!
+  @IBOutlet var arrivingTo: UILabel!
+  @IBOutlet var planeImage: UIImageView!
+  
+  @IBOutlet var flightStatus: UILabel!
+  @IBOutlet var statusBanner: UIImageView!
+  
+  //MARK: view controller methods
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    //adjust ui
+    summary.addSubview(summaryIcon)
+    summaryIcon.center.y = summary.frame.size.height/2
+    
+    //start rotating the flights
+    changeFlightDataTo(londonToParis)
+    
+    let rect = CGRect(x: 0.0, y: 100.0, width: view.bounds.width, height: 50.0)
+    let emitter = CAEmitterLayer()
+    emitter.frame = rect
+    view.layer.addSublayer(emitter)
+    // 动画的起始方向
+    emitter.emitterShape = kCAEmitterLayerRectangle
+    emitter.emitterPosition = CGPoint(x: rect.width/2, y: rect.height/2)
+    emitter.emitterSize = rect.size
+    
+    let emitterCell = CAEmitterCell()
+    emitterCell.contents = UIImage(named: "flake.png")!.CGImage
+    emitterCell.birthRate = 20
+    emitterCell.lifetime = 3.5
+    emitter.emitterCells = [emitterCell]
+    
+    emitterCell.yAcceleration = 70.0
+    emitterCell.xAcceleration = 10.0
+    emitterCell.velocity = 20.0
+    emitterCell.emissionLongitude = CGFloat(-M_PI_2)
+    
+    emitterCell.velocityRange = 200.0
+  }
+  
+  //MARK: custom methods
+  
+  func changeFlightDataTo(data: FlightData) {
+    
+    // populate the UI with the next flight's data
+    summary.text = data.summary as String
+    flightNr.text = data.flightNr as String
+    gateNr.text = data.gateNr as String
+    departingFrom.text = data.departingFrom as String
+    arrivingTo.text = data.arrivingTo as String
+    flightStatus.text = data.flightStatus as String
+    bgImageView.image = UIImage(named: data.weatherImageName as String)
+  }
+  
+  
+}
